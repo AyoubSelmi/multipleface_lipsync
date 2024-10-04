ec8da7b196388edb6f513cc2eae3957bc8120f70
Ayoub SELMI <ayoubselmii@gmail.com>
2024-10-04 11:26:42 +0100

    added lipsync bboxes videos

--- main.py
+++ main.py
@@ -2,24 +2,29 @@ import streamlit as st
 
 # Define paths to the video and audio files
 original_video_path = r"./input/multiface.mp4"
+
 translated_audio_path = r"./input/multiface.wav"
+
 lipsync_video_3_path = r"./output/output.mp4"
 lipsync_video_1_path = r"./output/video_out.mp4"
 lipsync_video_2_path = r"./output/video_speaking_out.mp4"
-
+lipsync_video_4_path = r"./output/lipsync_bbox2.mp4"
 
 original_video_path2 = r"./input/original.mp4"
+
 translated_audio_path2 = r"./input/original.wav"
+
 lipsync_video_3_path2 = r"./output/output2.mp4"
 lipsync_video_1_path2 = r"./output/video_out2.mp4"
 lipsync_video_2_path2 = r"./output/video_speaking_out2.mp4"
+lipsync_video_4_path2 = r"./output/lipsync_bbox2.mp4"
 
 
 def main():
     # Page configuration
     st.set_page_config(layout="wide")
     # First section: Original Video (centered and 50% width)
-    st.subheader("Original Video", anchor=None)
+    st.subheader("Original Video (one face per frame)", anchor=None)
     col1, col2, col3 = st.columns([1, 2, 1])  # Center the video by creating 3 columns
     with col2:
         st.video(original_video_path2)  # Let Streamlit control the width
@@ -43,14 +48,19 @@ def main():
         st.video(lipsync_video_2_path2)
 
     with col9:
-        st.text("Lipsync output")
-        st.video(lipsync_video_3_path2)
+        st.text("Lipsync output with asd bboxs")
+        st.video(lipsync_video_4_path)
 
+    st.subheader("Lipsync final output", anchor=None)
+    col1, col2, col3 = st.columns([1, 2, 1])  # Center the video by creating 3 columns
+    with col2:
+        st.video(lipsync_video_3_path2)  # Let Streamlit control the width
+    
     st.divider()
     st.divider()
 
     # First section: Original Video (centered and 50% width)
-    st.subheader("Original Video", anchor=None)
+    st.subheader("Original Video (multiple faces per frame)", anchor=None)
     col1, col2, col3 = st.columns([1, 2, 1])  # Center the video by creating 3 columns
     with col2:
         st.video(original_video_path)  # Let Streamlit control the width
@@ -74,9 +84,13 @@ def main():
         st.video(lipsync_video_2_path)
 
     with col9:
-        st.text("Lipsync output")
-        st.video(lipsync_video_3_path)
+        st.text("Lipsync output with asd bboxs")
+        st.video(lipsync_video_4_path2)
 
+    st.subheader("lipsync final output", anchor=None)
+    col1, col2, col3 = st.columns([1, 2, 1])  # Center the video by creating 3 columns
+    with col2:
+        st.video(lipsync_video_3_path)  # Let Streamlit control the width
 
 if __name__ == "__main__":
     main()
